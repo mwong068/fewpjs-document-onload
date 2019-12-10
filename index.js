e0078488c692d7f9c3088d6f6f2f7feba17566ce
mwong068 <mwong068@gmail.com>
2019-12-10 15:36:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+  document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    let replace = document.getElementById("text");
+    replace.innerHTML = "This is really cool!";
+  });
+   
